8f83c3f6fafa135c578c3650ee7be449b3962ca2
史俭朋 <shijianpeng@doushen.com>
2020-08-17 14:00:23 +0800

    optimize

--- expect_96.sh
+++ expect_96.sh
@@ -11,9 +11,9 @@ set ip 10.3.136.96
 # 执行的命令
 spawn ssh -p $port $user@$ip
 # 交互式输出
-expect "$user$ip's password:"
+expect "$user@$ip's password:"
 # 模拟输入密码，并且回车登录，这里有一定延迟，也可以手动自己回车
 send "$password\r"
 # 表示操作完成，用户继续可以交互操作
+# 或者使用 expect "*password:*" {send "$password\r;"}
 interact
-expect eof
